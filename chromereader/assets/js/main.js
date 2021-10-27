ddbad8acd1051f376b536746d73acd44c4761cf0
YsmGHa2099 <ztan0036@student.monash.edu>
2021-10-28 01:36:57 +1100

    cr

--- chromereader/assets/js/main.js
+++ chromereader/assets/js/main.js
@@ -9,49 +9,26 @@ $(document).ready(function() {
   });
     
   
+});
+
+
+$(document).ready(function() {
+  const win = $(window);
+  const doc = $(document);
+  const progressBar = $('progress');
+  const progressLabel = $('.progress-label');
+  const setValue = () => win.scrollTop();
+  const setMax = () => doc.height() - win.height();
+  const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
+  progressLabel.text(setPercent() + '%');
+  progressBar.attr({ value: setValue(), max: setMax() });
+  doc.on('scroll', () => {
+    progressLabel.text(setPercent() + '%');
+    progressBar.attr({ value: setValue() });
   });
+  win.on('resize', () => {
+    progressLabel.text(setPercent() + '%');
+    progressBar.attr({ value: setValue(), max: setMax() });
+  })
+});
 
-  
-(function () {
-    let fs = 3;
-    $(".c-f-s button")
-      .eq(0)
-      .click(function () {
-        fs++;
-        if (fs >= 5) {
-          fs = 5;
-          $(this).prop("disabled", true);
-          $("#page1-content-base").css("font-size", fs + "vw");
-          return;
-        }
-        $("#page1-content-base").css("font-size", fs + "vw");
-        $("button").prop("disabled", false);
-      });
-  
-    $(".c-f-s button")
-      .eq(1)
-      .click(function () {
-        fs--;
-        if (fs <= 1) {
-          fs = 1;
-          $(this).prop("disabled", true);
-          $("#page1-content-base").css("font-size", fs + "vw");
-          return;
-        }
-        $("#page1-content-base").css("font-size", fs + "vw");
-        $("button").prop("disabled", false);
-      });
-    $(".theme span").click(function () {
-      $(".root").attr("href", `./assets/css/root-${this.dataset["color"]}.css`);
-      if (
-        window.navigator.userAgent.includes("Safari") &&
-        !window.navigator.userAgent.includes("Chrome")
-      ) {
-        $(".root").attr(
-          "href",
-          `./assets/css/root-${this.dataset["color"]}-safari.css`
-        );
-      }
-    });
-
-})();
